28f6046a59be25c27229f34ddb2cd9691ab5609c
shaurya srivastav <40923031+TheHellfire99@users.noreply.github.com>
2019-10-11 10:23:27 -0700

    Add files via upload

---
+++ MPHA/js/script.js
@@ -0,0 +1,2 @@
+console.log("this website was made by shaurya srivastav of the team M.A.V.I, the team that built that drone");
+console.log("if you are reading this, you just found the easter egg!"); 
